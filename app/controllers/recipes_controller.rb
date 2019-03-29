d824e65470893b50edaa1c6968e7ecab0aaaa332
gbs4ever <gbs4ever2@gmail.com>
2019-03-29 11:52:50 -0400

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,9 +8,18 @@ class RecipesController < ApplicationController
   end
 
   def new
+    #binding.pry
     @recipe = Recipe.new
+    @recipe.ingredients.build
+    @recipe.ingredients.build
   end
 
   def create
+    var = Recipe.create(recipe_params)
+    redirect_to recipe_path(var)
+  end
+
+  def recipe_params
+      params.require(:recipe).permit(:title, ingredients_attributes: [ :name, :quantity])
   end
 end
